aca1c3e02a449a6ab508315510909a9394e735bc
Jan Segre <jan@vialink.com.br>
2013-09-12 15:38:12 -0300

    Ignore local config, distribute a sample.

--- config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-$rcmail_config['announce_migrations_new_skin']    = 'larry_maues';
-$rcmail_config['announce_migrations_old_skin']    = 'maues';
-$rcmail_config['announce_migrations_incremental'] = true;
-$rcmail_config['announce_migrations_min_version'] = 0;
-?>
